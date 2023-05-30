4fe4cf5290186266da920bd3b4b48903eb58271d
mangustik228 <bacek.mangust@gmail.com>
2023-05-30 16:07:51 +0000

    everything is allright

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import os
 from fastapi import FastAPI
+from dotenv import load_dotenv
+from loguru import logger
 
+load_dotenv()
 
 app = FastAPI()
 
 @app.get('/home')
 def home():
+    logger.info(f'{os.getenv("hello")}')
     return {'status':'ok','page':'home','app':'db'}
